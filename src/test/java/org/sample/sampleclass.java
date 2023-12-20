fd6d48f2f62de15c95c69a137d3b41052c8c7c7d
Krishnaaj@123 <kraj53937@gmail.com>
2023-12-20 15:51:12 +0530

    first commit

---
+++ src/test/java/org/sample/sampleclass.java
@@ -0,0 +1,35 @@
+package org.sample;
+
+public class sampleclass {
+    private void greensOmr() {
+        System.out.println("java");
+    }
+
+    private void greensAdayar() {
+        System.out.println("python");
+    }
+
+    private void greensTambaram() {
+        System.out.println("selenium");
+    }
+
+    private void greensVelacherry() {
+        System.out.println("Devops");
+    }
+
+    private void greensAnnaNagar() {
+        System.out.println("Spring Boot");
+    }
+
+    public static <GreensTech> void main(String[] args) {
+        sampleclass gt = new sampleclass();
+        gt.greensAdayar();
+        gt.greensAnnaNagar();
+        gt.greensOmr();
+        gt.greensTambaram();
+        gt.greensVelacherry();
+
+    }
+
+}
+
